668fa2c2914ae4a73ea5ff7193269fae5b7f8568
brother <brother@bsnet.se>
2013-11-15 09:24:56 +0000

    Commit from LXDE Pootle server by user brother.: 24 of 24 strings translated (0 fuzzy).

--- src/translations/lxinput-qt_template-sv.ts
+++ src/translations/lxinput-qt_template-sv.ts
@@ -1,82 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="sv">
-<context>
-    <name>MainDialog</name>
-    <message>
-        <source>Input Device Preferences</source>
-        <translation>Egenskaper för inmatningsenheter</translation>
-    </message>
-    <message>
-        <source>Motion</source>
-        <translation>Rörelse</translation>
-    </message>
-    <message>
-        <source>High</source>
-        <translation>Hög</translation>
-    </message>
-    <message>
-        <source>Fast</source>
-        <translation>Snabb</translation>
-    </message>
-    <message>
-        <source>Sensitivity:</source>
-        <translation>Känslighet:</translation>
-    </message>
-    <message>
-        <source>Low</source>
-        <translation>Låg</translation>
-    </message>
-    <message>
-        <source>Acceleration:</source>
-        <translation>Acceleration:</translation>
-    </message>
-    <message>
-        <source>Slow</source>
-        <translation>Långsam</translation>
-    </message>
-    <message>
-        <source>0</source>
-        <translation>0</translation>
-    </message>
-    <message>
-        <source>Left handed (Swap left and right mouse buttons)</source>
-        <translation>Vänsterhänt (byt vänster och höger musknapp)</translation>
-    </message>
-    <message>
-        <source>Mouse</source>
-        <translation>Mus</translation>
-    </message>
-    <message>
-        <source>Character Repeat</source>
-        <translation>Teckenupprepning</translation>
-    </message>
-    <message>
-        <source>Long</source>
-        <translation>Lång</translation>
-    </message>
-    <message>
-        <source>Repeat delay:</source>
-        <translation>Upprepningsfördröjning:</translation>
-    </message>
-    <message>
-        <source>Short</source>
-        <translation>Kort</translation>
-    </message>
-    <message>
-        <source>Repeat interval:</source>
-        <translation>Upprepningsintervall</translation>
-    </message>
-    <message>
-        <source>Type in the following box to test your keyboard settings</source>
-        <translation>Skriv i textrutan för att testa dina inställningar för tangentbordet</translation>
-    </message>
-    <message>
-        <source>Beep when there is an error of keyboard input</source>
-        <translation>Pip när det är fel vid tangentbordsinmatning</translation>
-    </message>
-    <message>
-        <source>Keyboard</source>
-        <translation>Tangentbord</translation>
-    </message>
-</context>
+<TS>
+    <context>
+        <name>../data/lxinput.ui.h:1</name>
+        <message>
+            <source>Input Device Preferences</source>
+            <translation>Egenskaper för inmatningsenhet</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:2</name>
+        <message>
+            <source>Sensitivity:</source>
+            <translation>Känslighet:</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:3</name>
+        <message>
+            <source>High</source>
+            <translation>Hög</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:4</name>
+        <message>
+            <source>Low</source>
+            <translation>Låg</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:5</name>
+        <message>
+            <source>Fast</source>
+            <translation>Snabb</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:6</name>
+        <message>
+            <source>Slow</source>
+            <translation>Långsam</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:7</name>
+        <message>
+            <source>Acceleration:</source>
+            <translation>Acceleration:</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:8</name>
+        <message>
+            <source>&lt;b&gt;Motion&lt;/b&gt;</source>
+            <translation>&lt;b&gt;Förflyttning&lt;/b&gt;</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:9</name>
+        <message>
+            <source>Left handed (Swap left and right mouse buttons)</source>
+            <translation>Vänsterhänt (byt funktioner för vänster och höger musknapp)</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:10</name>
+        <message>
+            <source>Mouse</source>
+            <translation>Mus</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:11</name>
+        <message>
+            <source>Delay before each key starts repeating</source>
+            <translation>Tid innan varje tangent repeteras</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:12</name>
+        <message>
+            <source>Repeat delay:</source>
+            <translation>Tid innan upprepning:</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:13</name>
+        <message>
+            <source>Interval between each key repeat</source>
+            <translation>Intervall mellan upprening av tangenter</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:14</name>
+        <message>
+            <source>Long</source>
+            <translation>Lång</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:15</name>
+        <message>
+            <source>Short</source>
+            <translation>Kort</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:16</name>
+        <message>
+            <source>Repeat interval:</source>
+            <translation>Tid mellan upprepningar:</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:17</name>
+        <message>
+            <source>Type in the following box to test your keyboard settings</source>
+            <translation>För att pröva dina inställningar kan du skriva i följande ruta</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:18</name>
+        <message>
+            <source>&lt;b&gt;Character Repeat&lt;/b&gt;</source>
+            <translation>&lt;b&gt;Teckenupprepning&lt;/b&gt;</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:19</name>
+        <message>
+            <source>Beep when there is an error of keyboard input</source>
+            <translation>Avge ett pip när ett fel uppstår vid tangentbordsinmatning</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:20</name>
+        <message>
+            <source>&lt;b&gt;Keyboard layout&lt;/b&gt;</source>
+            <translation>&lt;b&gt;Tangentbordsupplägg&lt;/b&gt;</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:21</name>
+        <message>
+            <source>Keyboard</source>
+            <translation>Tangentbord</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.ui.h:22</name>
+        <message>
+            <source>Touch Pad</source>
+            <translation>Pekplatta</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.desktop.in.h:1</name>
+        <message>
+            <source>Keyboard and Mouse</source>
+            <translation>Tangentbord och mus</translation>
+        </message>
+    </context>
+    <context>
+        <name>../data/lxinput.desktop.in.h:2</name>
+        <message>
+            <source>Configure keyboard, mouse, and other input devices</source>
+            <translation>Inställningar för tangentbord, mus och andra inmatningsenheter</translation>
+        </message>
+    </context>
 </TS>
